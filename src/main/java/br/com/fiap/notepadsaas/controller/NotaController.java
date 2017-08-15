242358ee7bd21ef0d26f609fd90566665062353f
logonrm <logonrm@fiap.com.br>
2017-08-15 20:54:42 -0300

    CRIADO O METODO DE findByTitulo

--- src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
@@ -3,27 +3,31 @@ package br.com.fiap.notepadsaas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.notepadsaas.model.Nota;
 import br.com.fiap.notepadsaas.repository.NotaRepository;
 
-@RestController 
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/nota")
 public class NotaController {
 
 	@Autowired
 	private NotaRepository notaRepository;
 
 	
-	@GetMapping(value="/")
+	@GetMapping
 	public List<Nota> findall(){
 		return notaRepository.findAll();
 	}
 
-	@PostMapping(value="/")
+	@GetMapping(value = "/titulo/{titulo}")
+	private List<Nota> findByTitulo(@PathVariable(value = "titulo") String q){
+		return notaRepository.findByTitulo(q);
+	}
+
+	@PostMapping
 	public void save(@RequestBody Nota nota){
 	notaRepository.save(nota);
 	
